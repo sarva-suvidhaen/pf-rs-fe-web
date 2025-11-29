3c3d5f0aba8ea2bb372c8255337cb0a808d2fce8
yashoda7 <gundayashoda7@gmail.com>
2025-11-29 10:57:34 +0530

    fixes:make fixes in findingstation function to correctly display the nearest station #1

---
+++ config.js
@@ -0,0 +1,16 @@
+// config.js
+
+// Change ENV value only (UAT / PROD)
+const ENV = "PROD";
+
+const CONFIG = {
+  UAT: {
+    BASE_URL: "https://rsmsuatapi.biputri.com/"
+  },
+  PROD: {
+    BASE_URL: "https://rsmsapi.biputri.com/"
+  }
+};
+
+// Exported value used everywhere
+const BASE_URL = CONFIG[ENV].BASE_URL;
